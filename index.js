b856c0d996dcb0f7ae065a962395af3b3c0c0069
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-04-17 17:14:57 -0500

    s3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-
+const uploadRouter = require("./routers/upload/upload-router")
 const server = express()
 var PORT = process.env.PORT || 5555
 
-
+server.use("/upload",uploadRouter)
 server.listen(PORT, () => {
     console.log(`=======Listening on ${PORT}=========`)
 })
